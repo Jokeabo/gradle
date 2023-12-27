aa5b1f34ff69034a6fd6d69b902b4df5a21eb082
Rafael Chaves <rchaves@gradle.com>
2023-12-27 13:56:19 -0300

    Add SupportsConvention/ConfigurableValue to properties
    Issue: #18352

--- platforms/core-configuration/model-core/src/main/java/org/gradle/api/internal/provider/MapCollectors.java
+++ platforms/core-configuration/model-core/src/main/java/org/gradle/api/internal/provider/MapCollectors.java
@@ -214,4 +214,57 @@ public class MapCollectors {
             return providerOfEntries.getProducer();
         }
     }
+
+
+    public static class PlusCollector<K, V> implements MapCollector<K, V> {
+        private final MapCollector<K, V> left;
+        private final MapCollector<K, V> right;
+
+        public PlusCollector(MapCollector<K, V> left, MapCollector<K, V> right) {
+            this.left = left;
+            this.right = right;
+        }
+
+        @Override
+        public boolean calculatePresence(ValueConsumer consumer) {
+            return left.calculatePresence(consumer) && right.calculatePresence(consumer);
+        }
+
+        @Override
+        public Value<Void> collectEntries(ValueConsumer consumer, MapEntryCollector<K, V> collector, Map<K, V> dest) {
+            Value<Void> leftValue = left.collectEntries(consumer, collector, dest);
+            if (leftValue.isMissing()) {
+                return leftValue;
+            }
+            Value<Void> rightValue = right.collectEntries(consumer, collector, dest);
+            if (rightValue.isMissing()) {
+                return rightValue;
+            }
+
+            return Value.present()
+                .withSideEffect(SideEffect.fixedFrom(leftValue))
+                .withSideEffect(SideEffect.fixedFrom(rightValue));
+        }
+
+        @Override
+        public Value<Void> collectKeys(ValueConsumer consumer, ValueCollector<K> collector, ImmutableCollection.Builder<K> dest) {
+            Value<Void> leftResult = left.collectKeys(consumer, collector, dest);
+            if (leftResult.isMissing()) {
+                return leftResult;
+            }
+            Value<Void> rightResult = right.collectKeys(consumer, collector, dest);
+            return rightResult.isMissing() ? leftResult : rightResult;
+        }
+
+        @Override
+        public void calculateExecutionTimeValue(Action<ExecutionTimeValue<? extends Map<? extends K, ? extends V>>> visitor) {
+            left.calculateExecutionTimeValue(visitor);
+            right.calculateExecutionTimeValue(visitor);
+        }
+
+        @Override
+        public ValueProducer getProducer() {
+            return left.getProducer().plus(right.getProducer());
+        }
+    }
 }
